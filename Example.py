037257d93470c6a0bdfdd3c22f5448743c225b49
Rafa <8rafael@gmail.com>
2018-09-11 14:41:13 +0100

    example for interactive graph

---
+++ Example.py
@@ -0,0 +1,24 @@
+from InteractiveGraph import *
+
+# ____________________________________________________________________________________________
+#
+def testInteractiveGraph():
+    """
+    Simple test
+    """        
+    def f(t, csi):
+        b = 1.2
+        c = .4
+        return csi + b * t + c * t * t
+
+    eq = Equation(f, ['t'], ['csi', 'b', 'c'])
+    g = InteractiveGraph(eq, xmin = -5, xmax = 5, ymin = -10, ymax = 10, vmin = -5, vmax = 5, scale = 'lin')
+    g.plotInteractivePyGraph()
+    g.plotStaticPyGraph(1)
+    js = g.assembleJS()
+    print(js)
+
+# ____________________________________________________________________________________________
+#
+if __name__ == '__main__':
+    testInteractiveGraph()
